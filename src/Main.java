7d824d1cdcbce758b0f392a27b2181f2d0e23d1b
CoolAleks <87146838+CoolAleks@users.noreply.github.com>
2021-08-28 19:37:04 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,6 @@ public class Main {
         int currentAccount = 100; // ввести сумма на счёте
         int incomeSum = 1000; //ввести сумму пополнения
         int bonusRate = 100;
-        int finalAccount;
         int totalBonus;
         if (incomeSum >= 1000) {
             totalBonus = incomeSum / bonusRate;
