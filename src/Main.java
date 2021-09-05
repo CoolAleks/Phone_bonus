edf0c77a017af25cd4438cbae3653f3f82c816bb
CoolAleks <87146838+CoolAleks@users.noreply.github.com>
2021-09-05 15:18:45 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        int currentAccount = 100; // ввести сумма на счёте
-        int incomeSum = 1000; //ввести сумму пополнения
-        int bonusRate = 100;
+        int currentAccount = 100; // ввести сумму на счёте
+        int incomeSum = 10000; //ввести сумму пополнения
+        int bonusRate=100;
         int totalBonus;
-        if (incomeSum >= 1000) {
+        int FinalAccount;
+        if (incomeSum > 1000) {
             totalBonus = incomeSum / bonusRate;
-            System.out.println("BonusSum: " + totalBonus);
         } else {
             totalBonus = 0;
-            System.out.println("BonusSum:" + totalBonus);
         }
+        FinalAccount=currentAccount+incomeSum+totalBonus;
+        System.out.println("BonusSum:" + totalBonus);
+        System.out.println("FinalAccount "+FinalAccount);
     }
 }
